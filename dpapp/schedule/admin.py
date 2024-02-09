9a1d7310ad8c161843c2d505ba8036027c9ef5ec
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-02-09 23:05:06 +0300

    feat(DP-00014): add stream model and add to admin panel

--- dpapp/schedule/admin.py
+++ dpapp/schedule/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Stream
 
 
+@admin.register(Stream)
+class StreamAdmin(admin.ModelAdmin):
+
+    list_display = ('number',)
--- dpapp/schedule/models.py
+++ dpapp/schedule/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Stream(models.Model):
+
+    class Meta:
+        verbose_name = 'Поток'
+        verbose_name_plural = 'Потоки'
+        ordering = ('number',)
+
+    number = models.CharField(max_length=30, verbose_name='Наименование потока', help_text='Пример: "1 поток", "2 поток"')
