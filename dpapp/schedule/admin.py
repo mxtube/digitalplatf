8909600afdf9f58a0a83494ebc0fa35d703cc2ed
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-02-09 23:08:15 +0300

    feat(DP-00015): add groupstream model and add to admin panel

--- dpapp/schedule/admin.py
+++ dpapp/schedule/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Stream
+from .models import Stream, GroupStream
 
 
 @admin.register(Stream)
 class StreamAdmin(admin.ModelAdmin):
 
     list_display = ('number',)
+
+
+@admin.register(GroupStream)
+class GroupStreamAdmin(admin.ModelAdmin):
+
+    list_display = ('stream', 'group',)
+    list_filter = ('group__department', )
--- dpapp/schedule/models.py
+++ dpapp/schedule/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from educationpart.models import Studygroup
 
 class Stream(models.Model):
 
@@ -9,3 +9,14 @@ class Stream(models.Model):
         ordering = ('number',)
 
     number = models.CharField(max_length=30, verbose_name='Наименование потока', help_text='Пример: "1 поток", "2 поток"')
+
+
+class GroupStream(models.Model):
+
+    class Meta:
+        verbose_name = 'Распределение потока'
+        verbose_name_plural = 'Распределение потоков'
+        ordering = ('stream',)
+
+    group = models.ForeignKey(Studygroup, on_delete=models.PROTECT, verbose_name='Группа', null=True, related_name='groupstream_group_to_studygroup_id_fkey')
+    stream = models.ForeignKey(Stream, on_delete=models.PROTECT, verbose_name='Поток', null=True, related_name='groupstream_stream_to_stream_id_fkey')
