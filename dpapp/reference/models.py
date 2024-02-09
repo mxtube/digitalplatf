ebd78837f338cd6df6f7e62d068a7b90d5cc3281
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-02-10 00:19:54 +0300

    feat(DP-00018): add reference status model and add to admin panel

--- dpapp/reference/models.py
+++ dpapp/reference/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = "Статусы"
+
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название', help_text='Наименование статуса')
+    # TODO: Добавить поле цвет, для окращивания строк в таблице "Справки". Можно использовать библиотеку django-colorfield (https://pypi.org/project/django-colorfield/)
+
+    def __str__(self):
+        return f'{self.name}'
