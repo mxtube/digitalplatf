84c5abbaf42b63d9b6c361871d7941858dcc0010
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-02-13 19:50:49 +0300

    feat(DP-00029): add login template and add accounts urls

--- dpapp/core/urls.py
+++ dpapp/core/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('', include('college.urls')),
     path('schedule/', include('schedule.urls')),
     path('reference/', include('reference.urls'))
