cef5641ca92066c55ea9fa08774849a1483de96d
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-02-14 11:14:29 +0300

    docs(DP-#): add comments for context proccessor

--- dpapp/college/context_proccessors.py
+++ dpapp/college/context_proccessors.py
@@ -2,6 +2,12 @@ from .models import SiteSettings, Department
 
 
 def load_settings(request):
+    """
+    The context with the site settings for the database.
+    Returns a list with objects.
+    :param request:
+    :return:
+    """
     return {'site_settings': SiteSettings.load()}
 
 
@@ -9,6 +15,7 @@ def departments(request):
     """
     The context for the base.
     Drop-down list of academic buildings menu categories of class schedules.
+    :param request:
     :return: list with all department objects
     """
     return {'departments': Department.objects.all()}
