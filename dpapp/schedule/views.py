9bf1116c65851e3fe11b3dd8be4a037303095840
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-02-15 00:30:36 +0300

    feat(DP-00036): add get method for ScheduleHome

--- dpapp/schedule/views.py
+++ dpapp/schedule/views.py
@@ -1,10 +1,16 @@
+import datetime
 from django.shortcuts import render
 from django.views import View
+from college.models import Department
 
 
 class ScheduleHome(View):
 
     template_name = 'schedule/index.html'
+    context = {'title': 'Расписание'}
 
     def get(self, request, pk):
-        return render(request, template_name=self.template_name)
+        department = Department.objects.get(pk=pk)
+        self.context['subtitle'] = department.short_name
+        self.context['date'] = datetime.date.today().strftime('%Y-%m-%d')
+        return render(request, template_name=self.template_name, context=self.context)
