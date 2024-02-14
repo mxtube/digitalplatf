c06bd4f4e031f3850087b2f56e0c5d0416fa7f69
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-02-14 23:01:41 +0300

    feat(DP-#): add check debug mode for database

--- dpapp/core/settings.py
+++ dpapp/core/settings.py
@@ -86,12 +86,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 # Auth
 # https://docs.djangoproject.com/en/5.0/topics/auth/customizing/
