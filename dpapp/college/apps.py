2febcb7f89f8a869ce6ce220f9e3421c2faa6600
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-03-11 18:47:44 +0300

    feat(DP-#): add func for create media folders after migrations

--- dpapp/college/apps.py
+++ dpapp/college/apps.py
@@ -1,7 +1,20 @@
+import os
+from core.settings import MEDIA_ROOT
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def create_media_folders(sender, **kwargs):
+    for i in ['img', 'json', 'pix', 'video', 'xls']:
+        if not os.path.exists(MEDIA_ROOT + i):
+            os.makedirs(MEDIA_ROOT + i)
 
 
 class CollegeConfig(AppConfig):
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'college'
     verbose_name = 'Колледж'
+
+    def ready(self):
+        post_migrate.connect(create_media_folders, sender=self)
