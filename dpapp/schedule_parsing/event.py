75378dfd51b1ab27f24400e318be58909f5d0f1e
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-03-14 21:47:11 +0300

    chore(DP-#): add pk to schedule calendar mark

--- dpapp/schedule_parsing/event.py
+++ dpapp/schedule_parsing/event.py
@@ -6,11 +6,11 @@ Created on Thu 7.03.24
 
 class Event:
     def __init__(self, group, couple, discipline, teacher, auditory):
-        self.group = group
-        self.couple = couple
-        self.discipline = discipline
-        self.teacher = teacher
-        self.auditory = auditory
+        self._group = group
+        self._couple = couple
+        self._discipline = discipline
+        self._teacher = teacher
+        self._auditory = auditory
 
     def __str__(self):
-        return f"{self.couple} {self.discipline} {self.teacher} {self.auditory}"
+        return f"{self._couple} {self._discipline} {self._teacher} {self._auditory}"
