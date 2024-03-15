574dc4d472b84fff4a1e964c1be905ae68c0b850
Кузнецов Кирилл Александрович <KAleksKuznetsov@sberbank.ru>
2024-03-15 10:21:23 +0300

    docs(DP-#): add comment for scheduling parser

--- dpapp/schedule_parsing/scheduling.py
+++ dpapp/schedule_parsing/scheduling.py
@@ -9,6 +9,7 @@ from core.settings import MEDIA_ROOT
 
 
 class Schedule:
+    """ Класс хранящий расписание для парсинга """
 
     JSON_PATH = MEDIA_ROOT + 'json/schedparsing/'
     IMAGE_PATH = MEDIA_ROOT + 'pix/schedparsing/'
@@ -17,6 +18,7 @@ class Schedule:
         self.schedule = {}
 
     def add_event(self, event):
+        """ Метод добавления элемента расписания в общий словарь с расписанием. """
         event = event.__dict__
         if self.__has_group(event.get('group')):
             if self.__add_couple(event.get('group'), event.get('couple')):
@@ -30,25 +32,31 @@ class Schedule:
         return self.schedule
 
     def __add_couple(self, group, couple):
+        """ Метод проверки номера пары в учебной группе. Если такой пары нет, добавляет. """
         if couple not in self.schedule.get(group):
             self.schedule[group][couple] = []
         return True
 
     def __has_group(self, group):
+        """ Метод проверки группы в расписании. Если такой нет, вызывает функцию добавления. """
         return self.__add_group(group) if group not in self.schedule.keys() else True
 
     def __add_group(self, group):
+        """ Метод добавления новой группы в расписание. """
         before = len(self.schedule.keys()) + 1
         self.schedule[group] = {}
         return True if len(self.schedule.keys()) - before == 1 else False
 
     def count_group(self):
+        """ Метод получения кол-ва групп в расписании """
         return len(self.schedule.keys())
 
     def display_schedule(self):
+        """ Метод получения расписания """
         return self.schedule
 
     def save_to_json(self):
+        """ Метод сохранения расписания в JSON """
         with open(self.JSON_PATH + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.json', 'w', encoding='utf-8') as f:
             json.dump(self.schedule, f, ensure_ascii=False, indent=4)
 
