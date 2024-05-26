b332e3ac445ed23deeb61d79ca0ed7fa81a74d73
Kirill Kuznetsov <mxtube@MacBook-Pro-Kirill.local>
2024-05-26 09:52:57 +0300

    feat(DP-00074): add article and category pages in docs

--- src/docs/views.py
+++ src/docs/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from .models import Article, Category
-from django.views.generic import ListView, DetailView, View
+from django.views.generic import ListView, DetailView
 
 
 class DocsHomePage(ListView):
@@ -20,10 +19,38 @@ class DocsHomePage(ListView):
 
 
 class CategoryPage(ListView):
-    pass
+
+    model = Article
+    template_name = 'docs/category.html'
+    context_object_name = 'articles'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'КП Справка'
+        context['subtitle'] = Category.objects.get(slug=self.kwargs['category_name']).name
+        return context
+
+    def get_queryset(self):
+        return Article.objects.filter(is_active=True, category__slug=self.kwargs['category_name']).order_by('name')
 
 
 class ArticlePage(DetailView):
-    pass
 
+    model = Article
+    template_name = 'docs/article.html'
+    context_object_name = 'article'
+    allow_empty = False
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'КП Справка'
+        context['subtitle'] = Category.objects.get(slug=self.kwargs['category_name']).name
+        return context
+
+    def get_object(self, *, object_list=None, **kwargs):
+        return Article.objects.get(
+            is_active=True,
+            slug=self.kwargs['article_name'],
+            category__slug=self.kwargs['category_name']
+        )
